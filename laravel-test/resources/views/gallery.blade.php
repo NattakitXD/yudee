a3d041e431345907b89ee279c4003afc55520da7
NattakitXD <nattakit.npithanom@gmail.com>
2024-01-23 17:02:18 +0700

    up

--- laravel-test/resources/views/gallery.blade.php
+++ laravel-test/resources/views/gallery.blade.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-    <script type="module" crossorigin="" src="css/gallerycss/theme.js"></script>
-    <link rel="stylesheet" href="{{ asset('/css/gallerycss/theme.css') }}" type="text/css" />
+    <script type="module" crossorigin="" src="/assets/js/galleryjs/theme.js"></script>
+    <link rel="stylesheet" href="{{ asset('/assets/css/gallerycss/theme.css') }}" type="text/css" />
 
     <script src="https://kit.fontawesome.com/8365c6ef59.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -1901,119 +1901,6 @@
     </main>
 
 
-    <footer>
-        <section class="bg-dark pt-5 pb-6 footer">
-            <div class="container ">
-
-
-
-                <div class="row justify-content-between bg-dark">
-                    <div class="col-md-6 col-lg-3 mb-6 mb-md-0">
-
-                        <a href="#">
-                            <img src="{{ URL('images/YuuDee2.png') }}" alt="" width="100"
-                                height="55" style="object-fit: contain;">
-                        </a>
-
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-secondary">58
-                                    Howard Street
-                                    #2 San Francisco</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary ">contact@homeid.com</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary">(+68)1221
-                                    09876</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary">www.homeid.com</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-6 col-lg-3 mb-6 mb-md-0">
-                        <h5 class="text-white">Popular Searches</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary ">Apartment
-                                    for Rent</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"class="nav-link p-0 text-secondary">Apartment
-                                    Low to hide</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary">Offices
-                                    for
-                                    Buy</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary">Offices
-                                    for
-                                    Rent</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-6 col-lg-3 mb-6 mb-md-0">
-                        <h5 class="text-white">Quick links</h5>
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-secondary">Terms
-                                    of
-                                    Use</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"class="nav-link p-0 text-secondary">Privacy
-                                    Policy</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"class="nav-link p-0 text-secondary">Contact
-                                    Support</a>
-                            </li>
-                            <li class="nav-item mb-2"><a href="#"
-                                    class="nav-link p-0 text-secondary">Careers</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-6 col-lg-3 mb-6 mb-md-0">
-                        <h5 class="text-white">Sign Up for Our Newsletter</h5>
-                        <p class="text-secondary">Lorem ipsum dolor sit amet, consecte tur cing elit. Suspe
-                            ndisse
-                            suscipit sagittis</p>
-                        <div class="input-group mb-3">
-                            <input id="newsletter1" type="text" class="form-control"
-                                placeholder="Email address" required>
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="submit">Subscribe</button>
-                            </div>
-                        </div>
-                        <i class="fa-brands fa-facebook"></i>
-                        <i class="fa-brands fa-twitter"></i>
-                        <i class="fa-brands fa-skype"></i>
-                    </div>
-                </div>
-
-                <p class="text-right">&copy; 2023 Company, Inc. All rights reserved.</p>
-                <ul class="list-unstyled d-flex">
-                    <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi"
-                                width="24" height="24">
-                                <use xlink:href="#twitter" />
-                            </svg></a></li>
-                    <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi"
-                                width="24" height="24">
-                                <use xlink:href="#instagram" />
-                            </svg></a></li>
-                    <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi"
-                                width="24" height="24">
-                                <use xlink:href="#facebook" />
-                            </svg></a></li>
-                </ul>
-
-            </div>
-        </section>
-
-    </footer>
-
     <div id="topButton" class="position-fixed pos-fixed-bottom-right p-6 z-index-10">
         <a href="#"
             class="gtf-back-to-top bg-white text-primary hover-white bg-hover-primary shadow p-0 w-52px h-52 rounded-circle fs-20 d-flex align-items-center justify-content-center in"
@@ -2067,11 +1954,11 @@
 
 </body>
 {{-- tabpanel --}}
-<script src="css/gallerycss/jquery.min.js"></script>
-<script src="css/gallerycss/bootstrap/bootstrap.bundle.js"></script>
+<script src="/assets/js/galleryjs/jquery.min.js"></script>
+<script src="/assets/js/galleryjs/bootstrap-js/bootstrap.bundle.js"></script>
 <script src="js/theme.js"></script>
 {{-- Image popup --}}
-<script src="css/gallerycss/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="/assets/js/galleryjs/magnific-popup-js/jquery.magnific-popup.min.js"></script>
 
 
 </html>
