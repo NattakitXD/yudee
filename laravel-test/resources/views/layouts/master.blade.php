60a42ffa132b3366826983c50114fb5001bb0355
NattakitXD <nattakit.npithanom@gmail.com>
2024-01-23 17:21:02 +0700

    up

--- laravel-test/resources/views/layouts/master.blade.php
+++ laravel-test/resources/views/layouts/master.blade.php
@@ -11,18 +11,19 @@
     <script src="https://kit.fontawesome.com/924b5e10e1.js" crossorigin="anonymous"></script>
     <link href='https://unpkg.com/css.gg@2.0.0/icons/css/chevron-down.css' rel='stylesheet'>
 
-    {{-- X --}}
+    {{-- X sidebar --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
      <script type="module" crossorigin="" src="/assets/js/ppjs/dashboard.js"></script>
     <script type="module" crossorigin="" src="/assets/js/ppjs/script.js"></script>
     <link rel="stylesheet" href="{{ asset('/assets/css/ppcss/dashboard.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/css/ppcss/theme.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('/assets/css/ppcss/themepp.css') }}" type="text/css">
       <!-- Include Select2 from CDN -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-
+    {{-- X sidebar --}}
+    {{-- X sidebar --}}
 
 
     {{-- --CSS BOOSTRAP-- --}}
